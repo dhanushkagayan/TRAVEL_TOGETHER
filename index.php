1254a2403ab5abb47ecae51dbf139101aeff7b22
Kadawatha <skydigitalweb01@gmail.com>
2018-03-26 16:33:21 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,473 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>TRAVEL TOGETHER</title>
+<!-- custom-theme -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Tourist a Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" /><!-- bootstrap-css -->
+
+<link href="css/owl.carousel.css" rel="stylesheet"><!-- Owl-carousel-CSS -->
+
+<link href="css/popup-box.css" rel="stylesheet" type="text/css" media="all" /><!-- pop-up css --> 
+
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+
+<!-- banner css -->
+<link rel="stylesheet" href="css/poposlides.css" type="text/css" media="all" />
+<!-- //banner css -->
+
+<!-- font-awesome-icons -->
+<link rel="stylesheet" href="css/font-awesome.min.css" />
+<!-- //font-awesome-icons -->
+
+<!-- google fonts -->
+<link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
+<!-- //google fonts -->
+
+</head>
+	
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+ 
+<!--/main-header-->
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Brand</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Tours</a></li>
+                <li><a href="#">Feed back</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+ <!--/banner-section-->
+
+<!-- about --> 
+<div class="w3ls-section w3-about" id="about">
+	<div class="container">
+		<div class="w3ls-title"> 
+			<h3 class="title">About us</h3>
+		</div>
+
+
+			<div class="col-md-4 about-top">
+				<h4>Welcome to travel tourist</h4>
+				<h5>travel tourist</h5>
+				<p>Pellentesque accumsan cursus dui, sodales blandit urna sodales vitae Pellentesque accumsan cursus dui, sodales blandit urna urna sodales vitae ipsum Pellentesque accumsan cursus dui, sodales blan urna sodales vitaePellentesque accumsan cursus dui, sodales blan sodales vitae.</p>
+				<a href="#" class="button button--nina" data-text="Learn more" data-toggle="modal" data-target="#myModal">Read More</a>
+			</div>
+			<div class="col-md-4 middle-img">
+				<img src="images/package1.jpg" alt="about image" />
+			</div>
+			<div class="col-md-4 about-top last">
+				<h4>Watch our recent video trip by travel tourist</h4>
+				<div class="text-center">
+					<a class="#" data-toggle="modal" data-target="#myModal1">
+						<span class="glyphicon glyphicon-play-circle" aria-hidden="true"></span>
+					</a>
+				</div>
+				<!-- Modal5 -->
+					<div class="modal fade" id="myModal1" tabindex="-1" role="dialog" >
+						<div class="modal-dialog">
+						<!-- Modal content-->
+							<div class="modal-content">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal">&times;</button> 
+									<!-- <iframe src="https://player.vimeo.com/video/88224399"></iframe>	-->
+									<iframe src="https://player.vimeo.com/video/92884601"></iframe>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- //Modal5 -->
+
+				<p>Pellentesque accumsan cursus dui, sodales blandit urna sodales vitae Pellentesque accumsan cursus dui, sodales blandit urna urna sodales vitae ipsum Pellentesque accumsan cursus dui, sodales blan urna sodales vitaePellentesque accumsan cursus dui, sodales blan sodales vitae.</p>
+			</div>
+			<div class="clearfix"></div>
+
+<div class="gap"></div>
+
+        <div class="book-form">
+            <form action="#" method="post">
+                <div class="col-md-3 form-time-w3layouts editContent">
+                    <label class="editContent"><span class="fa fa-user" aria-hidden="true"></span> Traveller Name</label>
+                    <input type="text" placeholder="Name" required="">
+                </div>
+                <div class="col-md-3 form-date-w3-agileits editContent">
+                    <label class="editContent"><span class="fa fa-map-marker" aria-hidden="true"></span> Travel place</label>
+                    <select class="form-control">
+                        <option>London</option>
+                        <option>Paris</option>
+                        <option>India</option>
+                        <option>Japan</option>
+                        <option>America</option>
+                    </select>
+                </div>
+                <div class="col-md-2 form-left-agileits-w3layouts editContent">
+                    <label class="editContent"><span class="fa fa-bus" aria-hidden="true"></span> Trip in</label>
+                    <div class="agileits_w3layouts_main_gridl">
+                        <input class="date has Datepicker" id="datepicker" name="Text" type="text" value="Start Date" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = '21/11/2017';}" required="">
+                    </div>
+                </div>
+                <div class="col-md-2 form-left-agileits-w3layouts editContent">
+                    <label class="editContent"><span class="fa fa-bus" aria-hidden="true"></span> Trip out</label>
+                    <div class="agileits_w3layouts_main_gridl">
+                        <input class="date has Datepicker" id="datepicker1" name="Text" type="text" value="Return Date" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = '21/11/2017';}" required="">
+                    </div>
+                </div>
+                <div class="col-md-2 form-left-agileits-submit editContent">
+                    <input type="submit" value="Send">
+                </div>
+                <div class="clearfix"></div>
+            </form>
+        </div>
+
+
+
+
+	</div>	
+</div>	
+<!-- //about -->
+ 
+<!-- travel -->
+<div class="travel">
+	<div class="container">
+		<h4>Travel tourist</h4>
+		<h3>Better to see something once than hear about it a thousand times</h3>
+		<p>watch our video here</p>
+		<div class="text-center">
+			<a class="#" data-toggle="modal" data-target="#myModal1">
+				<span class="glyphicon glyphicon-play-circle" aria-hidden="true"></span>
+			</a>
+		</div>
+	</div>
+</div>
+<!-- //travel -->
+
+<!-- popular packages -->
+<div class="packages" id="packages">
+	<div class="container">
+		<div class="wthree_head_section">
+			<h3 class="title">Popular Packages</h3>
+		</div>
+		<div class="packages_grids">
+			<div class="packages_grid1">
+				<div class="col-md-3 package1">
+					<img src="images/package1.jpg" alt="package1" />
+					<h4>London Holiday Tour</h4>
+					<h5>900$</h5>
+					<a href="#small-dialog" class="w3_agileits_sign_up2 popup-with-zoom-anim ab scroll">Book Now</a>
+				</div>
+				<div class="col-md-3 package1">
+					<img src="images/package2.jpg" alt="package1" />
+					<h4>Paris Eiffel Tower</h4>
+					<h5>290$</h5>
+					<a href="#small-dialog" class="w3_agileits_sign_up2 popup-with-zoom-anim ab scroll">Book Now</a>
+				</div>
+				<div class="col-md-6 package1">
+					<img src="images/package3.jpg" alt="package1" />
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="packages_grid2">
+				<div class="col-md-6 package1">
+					<img src="images/package6.jpg" alt="package1" />
+				</div>
+				<div class="col-md-3 package1">
+					<img src="images/package4.jpg" alt="package1" />
+					<h4>Thailand Summer Beach</h4>
+					<h5>850$</h5>
+					<a href="#small-dialog" class="w3_agileits_sign_up2 popup-with-zoom-anim ab scroll">Book Now</a>
+				</div>
+				<div class="col-md-3 package1">
+					<img src="images/package5.jpg" alt="package1" />
+					<h4>Japan Imperial Museum</h4>
+					<h5>210$</h5>
+					<a href="#small-dialog" class="w3_agileits_sign_up2 popup-with-zoom-anim ab scroll">Book Now</a>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- //popular packages -->
+ 
+
+
+<!-- footer start here -->
+<div class="footer-agile">
+
+	<!-- //newsletter -->
+		<div class="container">
+			<div class="footer-btm-agileinfo">
+				<div class="col-md-3 col-xs-3 footer-grid w3social">
+					<h3>About us</h3>
+					<p class="footer-p1">Lorem ipsum dolor sit amet, con sectetur adipiscing elit. Proin sed ligula 
+					ac metus finibus hendrerit sed at libero. Praesent reiciendis voluptatibus maiores alias.</p>
+					<a href="#">read more <span class="fa fa-long-arrow-right"></span></a>
+				</div>
+				<div class="col-md-3 col-xs-3 footer-grid">
+					<h3>Contact Info</h3>
+					<ul>
+						<li><i class="fa fa-phone"></i>+012 345 6789</li>
+						<li><i class="fa fa-fax"></i>+987 654 3210</li>
+						<li><i class="fa fa-map-marker"></i>Kmome St, NY 10002, Canada.</li>
+						<li><i class="fa fa-envelope-o"></i><a href="mailto:example@mail.com">mail@example.com</a></li>
+						<li><i class="fa fa-envelope-o"></i><a href="mailto:example@mail.com">mail1@example1.com</a></li>
+					</ul>
+				</div>
+				<div class="col-md-2 col-xs-3 footer-grid w3social">
+					<h3>Quick Links</h3>
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="#about" class="scroll">About</a></li>
+						<li><a href="#packages" class="scroll">Packages</a></li>
+						<li><a href="#testimonials" class="scroll">Happy Customers</a></li>
+						<li><a href="#contact" class="scroll">Contact Us</a></li>
+					</ul>
+				</div>
+				<div class="col-md-4 col-xs-3 footer-grid">
+					<h3>Latest Tweets</h3>
+					<ul class="tweet-agile">
+					<li>
+						<i class="fa fa-twitter-square" aria-hidden="true"></i>
+						<p class="tweet-p1"><a href="mailto:support@company.com">@example</a> sit amet consectetur adipiscing. <a href="#">http://ax.by/zzzz</a></p>
+						<p class="tweet-p2">Posted 3 days ago.</p>
+					</li>
+					<li>
+						<i class="fa fa-twitter-square" aria-hidden="true"></i>
+						<p class="tweet-p1"><a href="mailto:support@company.com">@example</a> sit amet consectetur adipiscing. <a href="#">http://cx.dy/zzzz</a></p>
+						<p class="tweet-p2">Posted 3 days ago.</p>
+					</li>
+				</ul>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="footer-agilem">
+				<div class="col-sm-8 col-xs-9 copy-w3lsright">
+					<p>© <?php echo date("Y");  ?> TRAVEL TOGETHER. All rights reserved | Design by <a href=""> SKY DIGITAL</a></p>
+				</div>
+				<div class="col-sm-4 col-xs-3 social-w3licon">
+					<a href="#" class="social-button twitter"><i class="fa fa-twitter"></i></a>
+					<a href="#" class="social-button facebook"><i class="fa fa-facebook"></i></a>
+					<a href="#" class="social-button google"><i class="fa fa-google-plus"></i></a>
+					<a href="#" class="social-button dribbble"><i class="fa fa-dribbble"></i></a>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+</div>
+<!-- //footer end here -->
+
+<!-- popup signup form -->
+	<div class="wthree_pop_up"> 
+		<div id="small-dialog" class="mfp-hide agile_signup_form">
+			<h2>BOOK OUR TRAVEL TOURIST</h2>
+			<form action="#" method="post">
+			
+				<div class="form-control"> 
+					<label class="header1">Traveller Name <span>:</span></label>
+					<input type="text" id="firstname" name="firstname" placeholder="Full Name" title="Please enter your Full Name" required="">
+				</div>
+				
+				<div class="form-control">
+					<label class="editContent">Travel Location <span>:</span></label>
+						<select class="form-control">
+							<option>London</option>
+							<option>Paris</option>
+							<option>India</option>
+							<option>Japan</option>
+							<option>America</option>
+						</select>
+				</div>
+
+				<div class="form-control">	
+					<label class="header1">Email Address <span>:</span></label>
+					<input type="email" id="email" name="email" placeholder="mail@example.com" title="Please enter a valid email" required="">
+				</div>
+
+				<div class="form-control">	
+					<label class="header1">Phone Number <span>:</span></label>
+					<input type="tel" id="tel" name="tel" placeholder="Phone number" title="Please enter a valid Phone number" required="">
+				</div>
+				
+				<div class="form-control">	
+					<label class="editContent">Journey start date <span>:</span></label>
+					<input class="date has Datepicker" id="datepicker" name="Text" type="text" value="Start Date" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = '21/11/2017';}" required="">
+				</div>
+				
+				<div class="form-control">	
+					<label class="editContent">Journey return date <span>:</span></label>
+					<input class="date has Datepicker" id="datepicker" name="Text" type="text" value="Return Date" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = '21/11/2017';}" required="">
+				</div>
+
+				<div class="form-control">
+					<label class="editContent">No: of travellers <span>:</span></label>
+						<select class="form-control">
+							<option>1</option>
+							<option>2</option>
+							<option>3</option>
+							<option>4</option>
+							<option>5 or more</option>
+						</select>
+				</div>
+				<div class="w3_submit">
+					<input type="submit" class="register" value="Submit">
+				</div>
+				
+			</form>
+		</div>
+	</div>
+	<script type="text/javascript">
+		window.onload = function () {
+			document.getElementById("password1").onchange = validatePassword;
+			document.getElementById("password2").onchange = validatePassword;
+		}
+		function validatePassword(){
+			var pass2=document.getElementById("password2").value;
+			var pass1=document.getElementById("password1").value;
+			if(pass1!=pass2)
+				document.getElementById("password2").setCustomValidity("Passwords Don't Match");
+			else
+				document.getElementById("password2").setCustomValidity('');	 
+				//empty string means no validation error
+		}
+	</script>
+<!-- //popup signup form -->
+
+<!-- js -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<!-- //js -->
+
+<!-- Stats-Number-Scroller-Animation-JavaScript -->
+<script src="js/waypoints.min.js"></script> 
+<script src="js/counterup.min.js"></script> 
+<script>
+	jQuery(document).ready(function( $ ) {
+		$('.counter').counterUp({
+			delay: 10,
+			time: 1000 
+		});
+	});
+</script>
+<!-- //Stats-Number-Scroller-Animation-JavaScript -->
+
+<!-- Calendar -->
+<link rel="stylesheet" href="css/jquery-ui.css" />
+	<script src="js/jquery-ui.js"></script>
+	<script>
+		$(function() {
+			$( "#datepicker,#datepicker1" ).datepicker();
+		});
+	</script>
+<!-- //Calendar -->
+		
+<!-- for banner js file-->
+<script src="js/poposlides.js"></script>
+	<script>
+		$(".slides").poposlides();
+	</script>
+<!-- //for banner js file-->
+
+<!-- pop-up-box -->
+		<script src="js/jquery.magnific-popup.js" type="text/javascript"></script>
+		<script>
+			$(document).ready(function() {
+				$('.popup-with-zoom-anim').magnificPopup({
+					type: 'inline',
+					fixedContentPos: false,
+					fixedBgPos: true,
+					overflowY: 'auto',
+					closeBtnInside: true,
+					preloader: false,
+					midClick: true,
+					removalDelay: 300,
+					mainClass: 'my-mfp-zoom-in'
+				});														
+			});
+		</script>
+<!--//pop-up-box -->
+
+<!-- requried-jsfiles-for owl -->	<!-- testimonials -->
+	<script src="js/owl.carousel.js"></script>
+	<script>
+		$(document).ready(function () {
+			$("#owl-demo2").owlCarousel({
+				items: 1,
+				lazyLoad: false,
+				autoPlay: true,
+				navigation: false,
+				navigationText: false,
+				pagination: true,
+			});
+		});
+	</script>
+<!-- //requried-jsfiles-for owl -->	<!-- //testimonials -->
+
+<!-- start-smoth-scrolling -->
+<script src="js/SmoothScroll.min.js"></script>
+
+<script type="text/javascript" src="js/move-top.js"></script>
+	<script type="text/javascript" src="js/easing.js"></script>
+	<script type="text/javascript">
+		jQuery(document).ready(function ($) {
+			$(".scroll").click(function (event) {
+				event.preventDefault();
+				$('html,body').animate({
+					scrollTop: $(this.hash).offset().top
+				}, 1000);
+			});
+		});
+	</script>
+
+	<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				};
+			*/
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+<!-- //here ends scrolling icon -->
+
+<!-- Scrolling Nav JavaScript --> 
+    <script src="js/scrolling-nav.js"></script>  
+<!-- //fixed-scroll-nav-js --> 
+		
+<!-- for bootstrap working -->
+	<script src="js/bootstrap.js"></script>
+<!-- //for bootstrap working -->
+
+</body>
+</html>
